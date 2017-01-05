671ddbe3ce218ac76ff0ead508760ced22d5b93d
Manh Dao <ducmanhdao92@gmail.com>
2017-01-05 00:55:51 +0000

    Adding exceptions

--- greengraph/greengraph.py
+++ greengraph/greengraph.py
@@ -6,16 +6,28 @@ from greengraph.googlemap import GoogleMap
 class GreenGraph(object):
 	def __init__(self, start, end):
 		"""Initialise greengraph with 'start' and 'end' locations """
-		self.start=start
-		self.end=end
 		self.geocoder=geopy.geocoders.GoogleV3(domain="maps.google.co.uk")
+
+		if self.geolocate(start):
+			self.start=start
+		if self.geolocate(end):
+			self.end=end	
 
 	def geolocate(self, place):
 		"""Return the geolocation of a given place"""
-		return self.geocoder.geocode(place,exactly_one=False)[0][1]
+		n = self.geocoder.geocode(place,exactly_one=False)
 
-	def location_sequence(self, start,end,steps):
+		if n:
+			return n[0][1]
+		else:
+			raise NameError("This place is not recognized by Google Map. Please check if the name is correct")
+
+
+	def location_sequence(self, start, end, steps):
 		"""Break distance between 'start' and 'end' into 'steps'"""
+		if steps <= 0:
+			raise ValueError("Number of steps must be positive")
+
 		lats = np.linspace(start[0], end[0], steps)
 		longs = np.linspace(start[1],end[1], steps)
 		return np.vstack([lats, longs]).transpose()
